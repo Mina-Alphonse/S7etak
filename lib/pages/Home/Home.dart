52bceda01f692e8d43cd521d925053287bf75828
Assad <andrew.assad.94@gmail.com>
2021-05-18 21:36:17 +0200

    HomePage drawer added ..

--- lib/pages/Home/Home.dart
+++ lib/pages/Home/Home.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:gp/pages/Home/Components/Body.dart';
 
-import '../../Size_Config.dart';
 import '../../constraints.dart';
-import '../../GlobalComponents/searchBarWidget.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
+List<String> asami = [
+  "fonsi 1",
+  "fonsi 2",
+  "fonsi 3",
+  "fonsi 4",
+  "fonsi 5",
+];
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-        body: SafeArea(
-
-          child: SingleChildScrollView(
-              child: Body()
+        backgroundColor: Colors.white,
+        // appBar: AppBar(
+        //
+        // ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                // height: MediaQuery.of(context).size.height / 3,
+                accountEmail: Text(
+                  "Testing Mail",
+                  style: TextStyle(color: kPrimaryLiteColor),
+                ),
+                accountName: Text(
+                  "Tesint Name",
+                  style: TextStyle(color: kPrimaryLiteColor),
+                ),
+                decoration: BoxDecoration(color: kliteColor),
+              ),
+              ListView.builder(
+                  itemCount: asami.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      trailing: Icon(Icons.icecream,color: kPrimaryLiteColor,),
+                      title: Text(
+                        asami[index],
+                        style: TextStyle(color: kPrimaryLiteColor),
+                      ),
+                      onTap: () {},
+                    );
+                  })
+            ],
           ),
-        )
-    );
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(child: Body()),
+        ));
   }
 }
